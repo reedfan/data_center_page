a57aeec2a2283aeb4b80bccca1269b074bd1f0a4
李森森 <lisensen@youotech.com>
2024-08-26 17:26:51 +0800

    1

--- src/views/manage/propertyCenter/dataMap.vue
+++ src/views/manage/propertyCenter/dataMap.vue
@@ -1,6 +1,70 @@
 <template>
-  <div class="manageMain">
-    <p>数据地图</p>
+  <div class="manageMain manageMainNoBack dataMap">
+    <p style="width: 100%; height: 40px; line-height: 40px; font-size: 18px; color: #333333; text-align: center; margin-top: 20px">数据资产地图</p>
+    <div class="TopUnit">
+      <div>
+        <div><i></i></div>
+        <p>
+          表
+          <el-popover placement="top-start" title="标题" width="200" trigger="hover" content="这是一段内容,这是一段内容,这是一段内容,这是一段内容。">
+            <i slot="reference" class="tipIcon"></i>
+          </el-popover>
+        </p>
+        <p>已治理：54</p>
+        <p>未治理：11154</p>
+      </div>
+      <div>
+        <div><i></i></div>
+        <p>
+          指标
+          <el-popover placement="top-start" title="标题" width="200" trigger="hover" content="这是一段内容,这是一段内容,这是一段内容,这是一段内容。">
+            <i slot="reference" class="tipIcon"></i>
+          </el-popover>
+        </p>
+        <p>数量：493</p>
+      </div>
+      <div>
+        <div><i></i></div>
+        <p>
+          标签
+          <el-popover placement="top-start" title="标题" width="200" trigger="hover" content="这是一段内容,这是一段内容,这是一段内容,这是一段内容。">
+            <i slot="reference" class="tipIcon"></i>
+          </el-popover>
+        </p>
+        <p>数量：25</p>
+      </div>
+      <div>
+        <div><i></i></div>
+        <p>
+          API
+          <el-popover placement="top-start" title="标题" width="200" trigger="hover" content="这是一段内容,这是一段内容,这是一段内容,这是一段内容。">
+            <i slot="reference" class="tipIcon"></i>
+          </el-popover>
+        </p>
+        <p>数量：73</p>
+      </div>
+      <div>
+        <div><i></i></div>
+        <p>
+          业务对象
+          <el-popover placement="top-start" title="标题" width="200" trigger="hover" content="这是一段内容,这是一段内容,这是一段内容,这是一段内容。">
+            <i slot="reference" class="tipIcon"></i>
+          </el-popover>
+        </p>
+        <p>数量：3</p>
+      </div>
+      <div>
+        <div><i></i></div>
+        <p>
+          专辑
+          <el-popover placement="top-start" title="标题" width="200" trigger="hover" content="这是一段内容,这是一段内容,这是一段内容,这是一段内容。">
+            <i slot="reference" class="tipIcon"></i>
+          </el-popover>
+        </p>
+        <p>公开：43</p>
+        <p>私有：13</p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -25,4 +89,5 @@ export default {
 </script>
 
 <style scoped>
+@import '../../../assets/manage/dataMap/dataMap.css';
 </style>
